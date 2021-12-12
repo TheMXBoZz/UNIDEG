8a718ef10705c99ba8edb725a688c92af782935f
TheMXBoZz <95950630+TheMXBoZz@users.noreply.github.com>
2021-12-12 12:40:12 -0600

    Actualizacion 12/12/2021

--- index.php
+++ index.php
@@ -24,6 +24,24 @@
             }
         </style>
 
+<script language="JavaScript">
+function mueveReloj(){
+momentoActual = new Date()
+hora = momentoActual.getHours()
+minuto = momentoActual.getMinutes()
+segundo = momentoActual.getSeconds()
+ampm = hora >= 12 ? 'pm' : 'am';
+hora = hora % 12;
+  hora = hora ? hora : 12; // the hour '0' should be '12'
+
+horaImprimible = hora + " : " + minuto + " : " + segundo
+
+document.form_reloj.reloj.value = horaImprimible
+
+setTimeout("mueveReloj()",1000,1)
+}
+</script>
+
         <title>SISTEMA DE INCIDENCIAS</title>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -31,119 +49,11 @@
     </head>
 
     <!-- Fondo -->
-    <body style="background-color:#297DE1;"> 
+    <body style="background-color:#297DE1;" onload="mueveReloj()"> 
 
 
     
-    <script type="text/javascript">
 
-// Establece el numero de escarcha
-var snowmax=35
-
-// Color de la nieve
-var snowcolor=new Array("#aaaacc","#ddddff","#ccccdd","#f3f3f3","#f0ffff")
-
-// Fuentes con que se crea la nieve
-var snowtype=new Array("Times","Arial","Times","Verdana")
-
-// Letra para crear la escarcha (en mi caso * )
-var snowletter="*"
-
-// Velocidad con que cae
-var sinkspeed=1
-
-// Tamaño maximo de la nieve
-var snowmaxsize=30
-
-// Tamaño minino de la nieve
-var snowminsize=8
-
-// Establecer la zona donde cae la nieve (1 todo, 2 izquierda, 3 centro, 4 derecha)
-var snowingzone=1
-
-///////////////////////////////////////////////////////////////////////////
-// FIN DE CONFIGURACION
-///////////////////////////////////////////////////////////////////////////
-
-
-// No edites bajo esta linea (POR FAVOR)
-var snow=new Array()
-var marginbottom
-var marginright
-var timer
-var i_snow=0
-var x_mv=new Array();
-var crds=new Array();
-var lftrght=new Array();
-var browserinfos=navigator.userAgent
-var ie5=document.all&&document.getElementById&&!browserinfos.match(/Opera/)
-var ns6=document.getElementById&&!document.all
-var opera=browserinfos.match(/Opera/)
-var browserok=ie5||ns6||opera
-
-function randommaker(range) {
-        rand=Math.floor(range*Math.random())
-    return rand
-}
-
-function initsnow() {
-        if (ie5 || opera) {
-                marginbottom = document.body.scrollHeight
-                marginright = document.body.clientWidth-15
-        }
-        else if (ns6) {
-                marginbottom = document.body.scrollHeight
-                marginright = window.innerWidth-15
-        }
-        var snowsizerange=snowmaxsize-snowminsize
-        for (i=0;i<=snowmax;i++) {
-                crds[i] = 0;
-            lftrght[i] = Math.random()*15;
-            x_mv[i] = 0.03 + Math.random()/10;
-                snow[i]=document.getElementById("s"+i)
-                snow[i].style.fontFamily=snowtype[randommaker(snowtype.length)]
-                snow[i].size=randommaker(snowsizerange)+snowminsize
-                snow[i].style.fontSize=snow[i].size+'px';
-                snow[i].style.color=snowcolor[randommaker(snowcolor.length)]
-                snow[i].style.zIndex=1000
-                snow[i].sink=sinkspeed*snow[i].size/5
-                if (snowingzone==1) {snow[i].posx=randommaker(marginright-snow[i].size)}
-                if (snowingzone==2) {snow[i].posx=randommaker(marginright/2-snow[i].size)}
-                if (snowingzone==3) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/4}
-                if (snowingzone==4) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/2}
-                snow[i].posy=randommaker(2*marginbottom-marginbottom-2*snow[i].size)
-                snow[i].style.left=snow[i].posx+'px';
-                snow[i].style.top=snow[i].posy+'px';
-        }
-        movesnow()
-}
-
-function movesnow() {
-        for (i=0;i<=snowmax;i++) {
-                crds[i] += x_mv[i];
-                snow[i].posy+=snow[i].sink
-                snow[i].style.left=snow[i].posx+lftrght[i]*Math.sin(crds[i])+'px';
-                snow[i].style.top=snow[i].posy+'px';
-
-                if (snow[i].posy>=marginbottom-2*snow[i].size || parseInt(snow[i].style.left)>(marginright-3*lftrght[i])){
-                        if (snowingzone==1) {snow[i].posx=randommaker(marginright-snow[i].size)}
-                        if (snowingzone==2) {snow[i].posx=randommaker(marginright/2-snow[i].size)}
-                        if (snowingzone==3) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/4}
-                        if (snowingzone==4) {snow[i].posx=randommaker(marginright/2-snow[i].size)+marginright/2}
-                        snow[i].posy=0
-                }
-        }
-        var timer=setTimeout("movesnow()",50)
-}
-
-for (i=0;i<=snowmax;i++) {
-        document.write("<span id='s"+i+"' style='position:absolute;top:-"+snowmaxsize+"'>"+snowletter+"</span>")
-}
-if (browserok) {
-        window.onload=initsnow
-}
-
-</script>
 
 
         <!-- Fondo -->
@@ -170,8 +80,7 @@ if (browserok) {
                     </li> 
                     <li class="nav-item"> 
                         <a class="nav-link" href="ViewINprofesor.php" target="miContenedor" style="color:#21E4C0">Ver resgistro de incidencias</a> 
-                    </li>                    
-                    
+                    </li>                               
                 </ul> 
 
                                       <!-- Añadir en href el link a el menu inicial -->
@@ -191,6 +100,10 @@ if (browserok) {
 
 
         <h1 align="center" class="texto"  style="color:#F3851E"; > Bienvenido al sistema de INCIDENCIAS UNIDEG </h1> 
+        
+        <form name="form_reloj">
+                    <input readonly="readonly" type="text" name="reloj" size="15">
+                        </form>
 
         
         <div class="m-3" style="height:900px;"> 
@@ -202,11 +115,11 @@ if (browserok) {
         <div class="card text-white bg-danger mb-3">
             <div class="card-header" class="texto" style="color:#F3A11E" ></div>
             <br /> <br />
-            <center><h2 class="texto" style="color:#F3A11E"> Realizado por: Alejandro Ortiz Rodríguez</h2> </center>
+            <center><h2 class="texto" style="color:#F3A11E"> </h2> </center>
             <hr> 
             <br /> 
 
-            <div class="texto" style="color:#89E321" >V 1.0
+            <div class="texto" style="color:#89E321" >V 1.0 - By: Alejandro Ortiz Rodríguez
             </div>
         </div>
 
